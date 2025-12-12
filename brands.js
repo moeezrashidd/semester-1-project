f4ef4bd917867c8a3bb30d87d050110d2e39a680
Moeez Rashid <moeezrashidd@gmail.com>
2025-12-13 03:49:00 +0430

    almost home page done

---
+++ brands.js
@@ -0,0 +1,61 @@
+[
+  {
+    "category": "Electronics",
+    "brands": [
+      "Haier",
+      "Dawlance",
+      "Orient",
+      "PEL",
+      "TCL",
+      "Infinix",
+      "Samsung"
+    ]
+  },
+  {
+    "category": "Food & Grocery",
+    "brands": [
+      "National Foods",
+      "Shan Foods",
+      "Nestlé Pakistan",
+      "Dalda Foods",
+      "K&N's",
+      "Tapal Tea",
+      "Mitchell's Fruit Farms"
+    ]
+  },
+  {
+    "category": "Fashion & Apparel",
+    "brands": [
+      "Khaadi",
+      "J. (Junaid Jamshed)",
+      "Sapphire",
+      "Nishat Linen",
+      "Gul Ahmed / Ideas",
+      "Maria B",
+      "Sana Safinaz"
+    ]
+  },
+  {
+    "category": "Home & Kitchen",
+    "brands": [
+      "Master MoltyFoam",
+      "Interwood",
+      "Westpoint",
+      "Enviro",
+      "Homeware",
+      "Chughtaiz"
+    ]
+  },
+  {
+    "category": "Health & Beauty",
+    "brands": [
+      "Saeed Ghani",
+      "Conatural",
+      "Hemani Herbals",
+      "Masarrat Misbah",
+      "Luscious",
+      "Vince Care",
+      "Rivaj UK"
+    ]
+  }
+]
